0c6e805a242e5f63f078fc1a593d442b4dccc975
kecoma1 <kecoma2@gmail.com>
2020-10-08 18:14:10 +0200

    Added some javascript and some pics
    Co-authored-by: marcos.bernuy@estudiante.uam.es <marcos.bernuy@estudiante.uam.es>

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function checkFields() {
+    for (i = 0; i < arguments.length; i++) {
+        if (document.getElementById(arguments[i]).value.length == 0) {
+            alert("Debe introducir información en todos los campos ");
+            break;
+        }
+    }
+}
