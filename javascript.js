39370f1c1d8ae7ec5c01cc9352be3515d70a4073
kecoma1 <kecoma2@gmail.com>
2020-10-13 15:01:50 +0200

    Added animations

--- javascript.js
+++ javascript.js
@@ -1,13 +1,1 @@
-function checkFields() {
-    for (i = 0; i < arguments.length; i++) {
-        if (document.getElementById(arguments[i]).value.length == 0) {
-            alert("Debe introducir información en todos los campos ");
-            break;
-        }
-    }
-}
 
-function jsonToTable() {
-    var catalogo = JSON.parse(catalogo.json);
-    catalogo[0];
-}
