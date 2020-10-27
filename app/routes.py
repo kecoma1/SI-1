374d70475cdf57cea0bee9ca0c18a91cd5a4fcf5
kecoma1 <kecoma2@gmail.com>
2020-10-27 16:41:50 +0100

    Apache doesn't login

--- app/routes.py
+++ app/routes.py
@@ -19,6 +19,18 @@ catalogue = json.loads(catalogue_data)
 # Variables para guardar el usuario logeado
 logged = False
 username_logged = None
+loaded_posters = False
+
+def load_url_posters():
+    global loaded_posters
+    if loaded_posters == False:
+        for film in catalogue['peliculas']:
+            url = url_for('static', filename=film['poster'])
+            film['poster'] = url
+            for actor in film['actores']:
+                url = url_for('static', filename=actor['foto'])
+                actor['foto'] = url
+        loaded_posters = True
 
 
 @app.route('/')
@@ -33,6 +45,7 @@ def index():
 @app.route("/sidenav.html", methods=['GET'])
 def sidenav():
     global logged
+    load_url_posters()
     return render_template('sidenav.html', logged=logged)
 
 
@@ -40,12 +53,6 @@ def sidenav():
 def topnav():
     global logged
     return render_template('topnav.html', logged=logged)
-
-
-@app.route("/bottonnav.html", methods=['GET'])
-def bottonnav():
-    return render_template('bottonnav.html')
-
 
 @app.route("/login.html", methods=['POST'])
 def login_page_POST():
@@ -87,8 +94,8 @@ def login_page_GET():
         username_logged = None
         if 'usuario' in session:
             session.pop('usuario', None)
-        
-        #Eliminando el carrito de la sesión
+
+        # Eliminando el carrito de la sesión
         for id in session:
             if id != 'usuario':
                 session[id] = 0
@@ -115,7 +122,7 @@ def signup_page():
         session['usuario'] = username
 
         # Comprobamos si existe el usuario
-        dir_path = homedir = os.path.expanduser("~")
+        dir_path = os.path.expanduser("~")
         dir_path += "/public_html/usuarios/"+username
         if os.path.exists(dir_path):
             return render_template('signup.html', title='signup', logged=logged, error="El usuario ya existe")
@@ -162,7 +169,7 @@ def historial():
         data = data.split(' ')
         saldo = data[4]
         f.close()
-        
+
         f = open(dir_path+"/historial.json", "r")
         if f is not None:
             historial_data = f.read()
@@ -187,23 +194,29 @@ def carrito():
                 while i < session[str(film['id'])]:
                     carrito_films.append(film)
                     i += 1
+        load_url_posters()
         return render_template('carrito.html', logged=logged, carrito_films=carrito_films)
     else:
         pass
+    load_url_posters()
     return redirect(url_for('carrito'))
 
 
 @app.route("/index/<id>", methods=['GET'])
 def film_detail(id):
     global catalogue, logged
+    load_url_posters()
     return render_template('filmDetail.html', film=catalogue['peliculas'][int(id)-1], logged=logged)
+
 
 @app.route("/cargar_categoria/<string:categoria>", methods=['GET'])
 def category(categoria):
     global catalogue
+    load_url_posters()
     return render_template('category.html', movies=catalogue['peliculas'], categoria=categoria)
 
-@app.route("/busqueda", methods=['POST', 'GET'])
+
+@app.route("/busqueda", methods=['POST'])
 def busqueda():
     global catalogue
     busqueda = request.form['search']
@@ -211,9 +224,11 @@ def busqueda():
     for film in catalogue['peliculas']:
         if busqueda in film['titulo']:
             peliculas.append(film)
+    load_url_posters()
     return render_template('busqueda.html', movies=peliculas)
 
 # Redirects desde index/<id>
+
 
 @app.route("/index.html", methods=['GET', 'POST'])
 @app.route("/index/index", methods=['GET'])
@@ -222,10 +237,12 @@ def busqueda():
 def redirect_index():
     return redirect(url_for('index'))
 
+
 @app.route("/realizar_compra/index/<id>", methods=['GET'])
 @app.route("/cargar_categoria/index/<id>", methods=['GET'])
 def redirect_filmDetail(id):
-    return redirect(url_for('film_detail', id= id))
+    return redirect(url_for('film_detail', id=id))
+
 
 @app.route("/cargar_categoria/login.html", methods=['GET'])
 @app.route("/realizar_compra/login.html", methods=['GET'])
@@ -233,11 +250,13 @@ def redirect_filmDetail(id):
 def redirect_login_page():
     return redirect(url_for('login_page_GET'))
 
+
 @app.route("/cargar_categoria/signup.html", methods=['GET'])
 @app.route("/realizar_compra/signup.html", methods=['GET'])
 @app.route("/index/signup.html", methods=['GET'])
 def redirect_signup_page():
     return redirect(url_for('signup_page_get'))
+
 
 @app.route("/realizar_compra/topnav.html", methods=['GET'])
 @app.route("/cargar_categoria/topnav.html", methods=['GET'])
@@ -245,17 +264,20 @@ def redirect_signup_page():
 def redirect_topnav():
     return render_template('topnav.html', logged=logged)
 
+
 @app.route("/realizar_compra/sidenav.html", methods=['GET'])
 @app.route("/cargar_categoria/sidenav.html", methods=['GET'])
 @app.route("/index/sidenav.html", methods=['GET'])
 def redirect_sidenav():
     return render_template('sidenav.html', logged=logged)
 
+
 @app.route("/cargar_categoria/historial.html", methods=['GET'])
 @app.route("/realizar_compra/historial.html", methods=['GET'])
 @app.route("/index/historial.html", methods=['GET'])
 def redirect_historial():
     return redirect(url_for('historial'))
+
 
 @app.route("/cargar_categoria/carrito.html", methods=['GET'])
 @app.route("/realizar_compra/carrito.html", methods=['GET'])
@@ -264,6 +286,7 @@ def redirect_carrito():
     return redirect(url_for('carrito'))
 
 # Rutas para el carrito
+
 
 @app.route("/index/anhadir_carrito/<string:id>", methods=['POST'])
 def anhadir_carrito(id):
@@ -301,12 +324,13 @@ def realizar_compra(id):
                 if id in session and str(film['id']) == id:
                     if saldo > float(film['precio']):
                         saldo = saldo - film['precio']
-                        
-                        # Eliminando información anterior del archivo
-                        f.seek(0)  
-                        f.truncate()  
 
-                        f.write(data[0]+" "+data[1]+" " +data[2]+" "+data[3]+" "+str(saldo))
+                        # Eliminando información anterior del archivo
+                        f.seek(0)
+                        f.truncate()
+
+                        f.write(data[0]+" "+data[1]+" " +
+                                data[2]+" "+data[3]+" "+str(saldo))
                         f.close()
                         f = open(dir_path+"/historial.json", "r+")
                         historial_data = f.read()
@@ -315,10 +339,11 @@ def realizar_compra(id):
                             historial = json.loads(historial_data)
                         else:
                             historial = []
-                       
+
                         historial.append(film)
                         now = datetime.datetime.now()
-                        historial[-1]['time'] = now.strftime("%Y-%m-%d %H:%M:%S")
+                        historial[-1]['time'] = now.strftime(
+                            "%Y-%m-%d %H:%M:%S")
                         if len(historial) > 1:
                             movement_id = historial[-2]['movement_id']+1
                             historial[-1]['movement_id'] = movement_id
@@ -332,19 +357,23 @@ def realizar_compra(id):
                         if str(film['id']) in session:
                             session[str(film['id'])] -= 1
                             if session[str(film['id'])] == 0:
-                                session.pop(str(film['id']), None)  
+                                session.pop(str(film['id']), None)
                             else:
                                 pass
 
                         carrito_films = cargar_films()
+                        load_url_posters()
                         return render_template('carrito.html', logged=logged, carrito_films=carrito_films, error="Compra realizada con exito")
-                        
+
                     else:
                         carrito_films = cargar_films()
+                        load_url_posters()
                         return render_template('carrito.html', logged=logged, carrito_films=carrito_films, error="No hay suficiente saldo")
-    
+
     carrito_films = cargar_films()
+    load_url_posters()
     return render_template('carrito.html', logged=logged, carrito_films=carrito_films, error="Haga login para comprar")
+
 
 def cargar_films():
     global catalogue
@@ -357,11 +386,13 @@ def cargar_films():
                 i += 1
     return carrito_films
 
+
 @app.route("/index/cargar_categoria/<string:categoria>", methods=['GET'])
 @app.route("/cargar_categoria/cargar_categoria/<string:categoria>", methods=['GET'])
 @app.route("/realizar_compra/cargar_categoria/<string:categoria>", methods=['POST'])
 def redirect_category(categoria):
     return redirect(url_for('category', categoria=categoria))
+
 
 @app.route("/index/busqueda", methods=['POST'])
 def redirect_busqueda():
@@ -371,11 +402,14 @@ def redirect_busqueda():
     for film in catalogue['peliculas']:
         if busqueda in film['titulo']:
             peliculas.append(film)
+    load_url_posters()
     return redirect(url_for('busqueda', movies=peliculas))
+
 
 @app.route("/realizar_compra/realizar_compra/<string:id>", methods=['POST'])
 def redirect_realizar_compra(id):
     return redirect(url_for('realizar_compra', id=id))
+
 
 @app.route("/introducir_saldo", methods=['POST'])
 def introducir_saldo():
