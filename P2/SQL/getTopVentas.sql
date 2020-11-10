0ea8009aadabbb1e8ac8a9d3484eb5bf16921dc6
kecoma1 <kecoma2@gmail.com>
2020-11-10 12:05:08 +0100

    getTopVentas improved

--- P2/SQL/getTopVentas.sql
+++ P2/SQL/getTopVentas.sql
@@ -3,8 +3,8 @@ DROP FUNCTION getTopVentas (year_1 INTEGER, year_2 INTEGER);
 CREATE OR REPLACE FUNCTION getTopVentas (year_1 INTEGER, year_2 INTEGER) 
 RETURNS TABLE (
 ANO		TEXT,
-VENTAS		INTEGER,
-PELICULA	VARCHAR
+PELICULA	VARCHAR,
+VENTAS		INTEGER
 )
 AS $$
 
@@ -13,26 +13,22 @@ i integer:= 0;
 
 BEGIN
 
-IF (year_1 < year_2) then
-	i := year_1;
-ELSE
-	i := year_2;
-END IF;
-
-WHILE i<=year_2 LOOP
 	RETURN QUERY
-	SELECT a.year as ano, MAX(c.sales) as max_sales, a.movietitle as titulo
-			FROM imdb_movies as a, products as b, inventory as c
-			WHERE a.movieid = b.movieid AND c.prod_id = b.prod_id AND CAST(a.year AS INT) = i
-			GROUP BY a.year, a.movietitle
-			ORDER BY max_sales DESC
-			LIMIT 1;
-	i:= i+1;
-END LOOP;
+	SELECT t.ano, t.titulo as pelicula, MAX(t.max_sales) as ventas
+		FROM 	(	
+			SELECT a.year as ano, MAX(c.sales) as max_sales, a.movietitle as titulo,
+			ROW_NUMBER() OVER(PARTITION BY a.year ORDER BY MAX(c.sales) DESC) AS rk
+				FROM imdb_movies as a, products as b, inventory as c
+				WHERE a.movieid = b.movieid AND c.prod_id = b.prod_id AND CAST(a.year AS INTEGER) BETWEEN year_1 AND year_2
+				GROUP BY a.year, a.movietitle
+				ORDER BY max_sales DESC
+			) as t
+WHERE t.rk = 1
+GROUP BY t.ano, t.titulo
+ORDER BY ventas DESC;
 
 END; $$
 
 LANGUAGE 'plpgsql';
 
-SELECT * FROM getTopVentas(1995, 2000) as v ORDER BY v.ventas DESC
-
+SELECT * FROM getTopVentas(1985, 1990)
