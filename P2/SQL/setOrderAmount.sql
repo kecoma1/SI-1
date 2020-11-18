8579d74288f0a55398afd952677ed609de8955e5
kecoma1 <kecoma2@gmail.com>
2020-11-18 18:30:49 +0100

    setOrderAmount fixed

--- P2/SQL/setOrderAmount.sql
+++ P2/SQL/setOrderAmount.sql
@@ -9,16 +9,21 @@ BEGIN
 	SET 	netamount = t.sumprice,
 		totalamount = t.sumprice+t.sumprice*(tax/100)
 	FROM 	(
-			SELECT sum(b.price) AS sumprice, a.orderid AS ord_id
-			FROM orders AS a, orderdetail AS b
-			WHERE b.orderid = a.orderid 
-			GROUP BY a.orderid
+			--Consulta para obtener el precio total de cada venta
+			SELECT sum(price_by_quantity.prc_of_each_detail) AS sumprice, price_by_quantity.order_id_per_detail AS ord_id
+			FROM orders AS a, (
+						--Consulta para obtener el precio*quantity de cada producto
+						SELECT price*quantity as prc_of_each_detail, orderid as order_id_per_detail 
+						FROM orderdetail
+					) as price_by_quantity
+			WHERE price_by_quantity.order_id_per_detail = a.orderid 
+			GROUP BY a.orderid, price_by_quantity.order_id_per_detail
 		) AS t
-	WHERE 	t.ord_id = a.orderid AND (totalamount IS NULL OR netamount IS NULL);
+	WHERE 	t.ord_id = a.orderid AND (totalamount IS NULL AND netamount IS NULL);
 
 END;
 $$;
 
 CALL setOrderAmount();
 
-select * from orders
+select * from orders 
