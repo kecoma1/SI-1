3a94dd2bd84e94cb9039960a36dd22cad3403c06
kecoma1 <kecoma2@gmail.com>
2020-11-22 19:24:01 +0100

    updOrders working

--- P2/SQL/updOrders.sql
+++ P2/SQL/updOrders.sql
@@ -1,23 +1,38 @@
 DROP TRIGGER IF EXISTS updOrders ON orderdetail;
 
-CREATE OR REPLACE FUNCTION setOrderAmountId() 
-RETURNS TRIGGER 
-AS $$
-    BEGIN
-	IF (TG_OP = 'INSERT' OR TG_OP = 'UPDATE') THEN
+CREATE OR REPLACE FUNCTION updOrders()
+RETURNS TRIGGER AS
+$$
+	BEGIN
+		IF (TG_OP = 'INSERT' OR TG_OP = 'UPDATE' OR TG_OP = 'DELETE') THEN
 		UPDATE orders
-        SET netamount = t.precio
-        FROM    (
-                    SELECT SUM(a.price) AS precio, b.orderid AS id
-                    FROM orderdetail AS a, orders AS b
-                    WHERE a.orderid = b.orderid
-                ) AS t
-        WHERE NEW.orderid = t.id
-        RETURN NULL
-    ELSE
-        RETURN NULL
+		-- Actualizamos el valor con el precio correcto
+		SET netamount = t.precio, totalamount = t.precio+t.precio*((tax/100))
+		FROM    
+			(
+			-- Calculamos el total de la orden
+			SELECT SUM(t0.total) as precio
+			FROM	(
+					-- Calculamos precio por producto
+					SELECT quantity*price AS total 
+					FROM orderdetail 
+					WHERE orderid = NEW.orderid
+				) as t0
+			) AS t;
+			
+			RETURN NEW;
+		ELSE
+			RETURN NULL;
+		END IF;
+	END;
+$$
+LANGUAGE plpgsql;
 
 CREATE TRIGGER updOrders
 AFTER UPDATE OR INSERT OR DELETE ON orderdetail
 FOR EACH ROW
-EXECUTE PROCEDURE setOrderAmountId();
+EXECUTE PROCEDURE updOrders();
+
+select * from orderdetail where orderid = 1 limit 10
+select * from orders where orderid = 1
+update orderdetail set price = 3 where orderid = 1
