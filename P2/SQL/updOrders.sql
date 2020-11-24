faeb604c5fb046d50b147789be99a8b3467c19a3
kecoma1 <kecoma2@gmail.com>
2020-11-24 12:46:29 +0100

    There are sintax errors in updortders

--- P2/SQL/updOrders.sql
+++ P2/SQL/updOrders.sql
@@ -1,27 +1,49 @@
+
 DROP TRIGGER IF EXISTS updOrders ON orderdetail;
 
 CREATE OR REPLACE FUNCTION updOrders()
 RETURNS TRIGGER AS
 $$
 	BEGIN
-		IF (TG_OP = 'INSERT' OR TG_OP = 'UPDATE' OR TG_OP = 'DELETE') THEN
-		UPDATE orders
-		-- Actualizamos el valor con el precio correcto
-		SET netamount = t.precio, totalamount = t.precio+t.precio*((tax/100)),
-		FROM    
-			(
-			-- Calculamos el total de la orden
-			SELECT SUM(t0.total) as precio
-			FROM	
+		IF (TG_OP = 'INSERT' OR TG_OP = 'UPDATE') THEN
+			UPDATE orders
+			-- Actualizamos el valor con el precio correcto
+			SET netamount = t.precio, totalamount = t.precio+t.precio*((tax/100))
+			FROM    
 				(
-					-- Calculamos precio total de producto*cantidad
-					SELECT quantity*price AS total 
-					FROM orderdetail 
-					WHERE orderid = NEW.orderid
-				) as t0
-			) AS t;
-		WHERE NEW.orderid = orderid
+				-- Calculamos el total de la orden
+				SELECT SUM(t0.total) AS precio
+				FROM	
+					(
+						-- Calculamos precio total de producto*cantidad
+						SELECT quantity*price AS total 
+						FROM orderdetail 
+						WHERE orderid = NEW.orderid
+					) as t0
+				) AS t
+				WHERE NEW.orderid = orderid;
+				
 			RETURN NEW;
+			-- En el caso de que borremos tenemos que no tener en cuenta el id del product borrado
+		ELSE IF (TG_OP = 'DELETE') THEN
+			UPDATE orders
+			SET netamount = t.precio, totalamount = t.precio+t.precio*((tax/100))
+			FROM
+				(
+				-- 
+				SELECT SUM(t0.total) AS precio
+				FROM	
+					(
+						-- Calculamos precio total de producto*cantidad
+						SELECT quantity*price AS total 
+						FROM orderdetail 
+						-- Excluimos el producto borrado
+						WHERE orderid = NEW.orderid AND prod_id != NEW.prod_id
+					) as t0
+				) AS t
+				WHERE NEW.orderid = orderid;
+				
+			RETURN NEW;		
 		ELSE
 			RETURN NULL;
 		END IF;
