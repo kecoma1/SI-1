514bf0078bd66a334ea9d2e8e0bcefeec59e18cd
Marcos Bernuy <47560348+mbernuy@users.noreply.github.com>
2020-11-26 19:17:37 +0100

    Actualiza pasado a español

--- P2/SQL/actualiza.sql
+++ P2/SQL/actualiza.sql
@@ -1,12 +1,12 @@
-------- FIXING MISTAKES RELATED TO MISSING KEYS AND DUPLICATES --------
+------- SOLUCIONANDO ERRORES DE CLAVES QUE FALTAN Y DUPLICACDOS --------
 
--- To avoid problems in setPrice.sql
+-- Para evitar problemas con setPrice.sql
 update imdb_movies set year = '1999' where year = '1998-1999';
 
--- Add foreign key in orders
+-- Añadir claves externas en orders
 alter table orders add foreign key (customerid) references customers(customerid) on delete set null on update cascade;
 
--- Delete all the duplicates in orderdetail to later on set orderid and prod_id as primary keys
+-- Eliminar todos los duplicados en orderdetail para después poner orderid y prod_id como claves primarias
 delete from orderdetail where orderid in (
     select orderid
     from (
@@ -16,54 +16,55 @@ delete from orderdetail where orderid in (
     where t.row_num > 1
 );
 
--- Set primary key and foreign keys of orderdetail
+-- Establecer la clave primaria y secundaria de orderdetail
 alter table orderdetail add primary key (orderid, prod_id);
 alter table orderdetail add foreign key (orderid) references orders(orderid) on delete cascade on update cascade;
 alter table orderdetail add foreign key (prod_id) references products(prod_id) on delete cascade on update cascade;
 
--- Eliminate the primary key constraint in inventory
+-- Eliminar la restricción de clave primaria de inventory
 alter table inventory drop constraint inventory_pkey;
 
--- Set prod_id in inventory as foreign key
+-- Establecer prod_id en inventory como clave externa
 alter table inventory add foreign key (prod_id) references products(prod_id) on delete cascade on update cascade;
 
--- Add not null constraint to year in imdb_movies
+-- Añadir la restricción not null a año en imdb_movies
 alter table imdb_movies alter column year set not null;
 
--- Resetting the primary key value correctly
+-- Estableciendo la clave primaria de imdb_movielanguages correctamente
 alter table imdb_movielanguages drop constraint imdb_movielanguages_pkey;
 alter table imdb_movielanguages add primary key (movieid, language);
 
--- Set the primary key and foreign keys of imdb_actormovies
+-- Establecer la clave primaria y secundaria de imdb_actormovies
 alter table imdb_actormovies add primary key (actorid, movieid);
 alter table imdb_actormovies add foreign key (actorid) references imdb_actors(actorid) on delete cascade on update cascade;
 alter table imdb_actormovies add foreign key (movieid) references imdb_movies(movieid) on delete cascade on update cascade;
 
------- SETTING TO CASCADE ALL THE FOREIGN KEYS THAT WEREN'T AND MUST BE -------
+------ ESTABLECIENDO A CASCADE TODAS LAS CLAVES EXTERNAS QUE NO LO ESTÁN Y QUE DEBERÍAN -------
 
--- Set the foreign key of imdb_movies on cascade
+-- Establecer la clave externa de imdb_movies con cascade
 alter table products drop constraint products_movieid_fkey;
 alter table products add foreign key (movieid) references imdb_movies(movieid) on delete cascade on update cascade;
 
--- Set the foreign key of imdb_movieslanguages on cascade
+-- Establecer la clave externa de imdb_movielanguages con cascade
 alter table imdb_movielanguages drop constraint imdb_movielanguages_movieid_fkey;
 alter table imdb_movielanguages add foreign key (movieid) references imdb_movies(movieid) on delete cascade on update cascade;
 
--- Set the foreign key of imdb_moviescountries on cascade
+-- Establecer la clave externa de imdb_moviecountries con cascade
 alter table imdb_moviecountries drop constraint imdb_moviecountries_movieid_fkey;
 alter table imdb_moviecountries add foreign key (movieid) references imdb_movies(movieid) on delete cascade on update cascade;
 
--- Set the foreign key of imdb_moviesgenres on cascade
+-- Establecer la clave externa de imdb_moviegenres con cascade
 alter table imdb_moviegenres drop constraint imdb_moviegenres_movieid_fkey;
 alter table imdb_moviegenres add foreign key (movieid) references imdb_movies(movieid) on delete cascade on update cascade;
 
--- Set the foreign key of imdb_directormovies on cascade
+-- Establecer las claves externas de imdb_directormovies con cascade
 alter table imdb_directormovies drop constraint imdb_directormovies_movieid_fkey;
 alter table imdb_directormovies add foreign key (movieid) references imdb_movies(movieid) on delete cascade on update cascade;
 alter table imdb_directormovies add foreign key (directorid) references imdb_directors(directorid) on delete cascade on update cascade;
 
 ----------- SOLUCIONANDO INTEGRIDAD DEL DISEÑO --------------
 
+-- Creando la tabla imdb_language e insertando los valores
 create table imdb_language(
     language varchar primary key
 );
@@ -73,6 +74,7 @@ insert into imdb_language(
     from imdb_movielanguages
 );
 
+-- Creando la tabla imdb_genre e insertando los valores
 create table imdb_genre(
     genre varchar primary key
 );
@@ -82,6 +84,7 @@ insert into imdb_genre(
     from imdb_moviegenres
 );
 
+-- Creando la tabla imdb_country e insertando los valores
 create table imdb_country(
     country varchar primary key
 );
@@ -91,7 +94,7 @@ insert into imdb_country(
     from imdb_moviecountries
 );
 
--- Setting all the needed foreign keys
+-- Estableciendo las claves externas de las tablas creadas
 
 alter table imdb_movielanguages add foreign key (language) references imdb_language(language) on delete cascade on update cascade;
 
@@ -99,7 +102,7 @@ alter table imdb_moviecountries add foreign key (country) references imdb_countr
 
 alter table imdb_moviegenres add foreign key (genre) references imdb_genre(genre) on delete cascade on update cascade;
 
--- Creating the table alert
+-- Creando la tabla alerta
 create table alerta(
     prod_id int references products(prod_id) on delete cascade on update cascade,
     description text not null,
